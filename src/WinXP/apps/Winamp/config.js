02a8f651804e1472742f11124ba7ab627b35b0b5
bilaldemir <18368833+bilalvdemir@users.noreply.github.com>
2025-06-12 17:18:04 +0300

    add music

--- src/WinXP/apps/Winamp/config.js
+++ src/WinXP/apps/Winamp/config.js
@@ -5,7 +5,7 @@ const album = 'netBloc Vol. 24: tiuqottigeloot';
 export const initialTracks = [
   {
     url:
-      'https://raw.githubusercontent.com/bilalvdemir/winamp-xp/refs/heads/master/demo/mp3/CEZA%20-%20Neyim%20Var%20ki%20feat.%20Sagopa%20Kajmer.mp3',
+      'https://raw.githubusercontent.com/bilalvdemir/winXP/refs/heads/master/src/assets/sounds/CEZA-Neyim-Var-ki-feat-Sagopa-Kajmer.mp3',
     duration: 207,
     metaData: {
       title: 'Neyim Var ki feat. Sagopa Kajmer',
@@ -15,7 +15,7 @@ export const initialTracks = [
   },
   {
     url:
-      'https://raw.githubusercontent.com/bilalvdemir/winamp-xp/refs/heads/master/demo/mp3/Asl%C4%B1%20Gu%CC%88ngo%CC%88r%20-%20As%CC%A7k%20Hers%CC%A7eye%20Deg%CC%86er.mp3',
+      'https://raw.githubusercontent.com/bilalvdemir/winXP/refs/heads/master/src/assets/sounds/Asli-Gungor-Ask-Herseye-Deger.mp3',
     duration: 224,
     metaData: {
       title: 'Aşk Herşeye Değer',
@@ -25,7 +25,7 @@ export const initialTracks = [
   },
   {
     url:
-      'https://raw.githubusercontent.com/bilalvdemir/winamp-xp/refs/heads/master/demo/mp3/Asl%C4%B1%20Gu%CC%88ngo%CC%88r-Ferhat%20Go%CC%88c%CC%A7er%20-%20Kalp%20Kalbe%20Kars%CC%A7%C4%B1%20(Sinan%20Akc%CC%A7il%20Mix).mp3',
+      'https://raw.githubusercontent.com/bilalvdemir/winXP/refs/heads/master/src/assets/sounds/Aslı-Gungor-Ferhat-Gocer-Kalp -Kalbe-Karsi-(Sinan-Akcil-Mix).mp3',
     duration: 249,
     metaData: {
       title: 'Kalp Kalbe Karşı (Sinan Akçil Mix)',
@@ -35,17 +35,17 @@ export const initialTracks = [
   },
   {
     url:
-      'https://raw.githubusercontent.com/bilalvdemir/winamp-xp/refs/heads/master/demo/mp3/Ayc%CC%A7a%20-%20Y%C4%B1k%C4%B1l%C4%B1yo%20(Official%20Video).mp3',
+      'https://raw.githubusercontent.com/bilalvdemir/winXP/refs/heads/master/src/assets/sounds/Ayca-Yikiliyo.mp3',
     duration: 252,
     metaData: {
-      title: 'Yıkılıyo (Official Video)',
+      title: 'Yıkılıyo (Official Audio)',
       artist: 'Ayça',
       album,
     },
   },
   {
     url:
-      'https://raw.githubusercontent.com/bilalvdemir/winamp-xp/refs/heads/master/demo/mp3/Ayna%20-%20Severek%20Ayr%C4%B1lanlar.mp3',
+      'https://raw.githubusercontent.com/bilalvdemir/winXP/refs/heads/master/src/assets/sounds/Ayna-Severek-Ayrilanlar.mp3',
     duration: 297,
     metaData: {
       title: 'Severek Ayrılanlar',
@@ -55,7 +55,7 @@ export const initialTracks = [
   },
   {
     url:
-      'https://raw.githubusercontent.com/bilalvdemir/winamp-xp/refs/heads/master/demo/mp3/Britney%20Spears%20-%20Toxic%20(Official%20HD%20Video).mp3',
+      'https://raw.githubusercontent.com/bilalvdemir/winXP/refs/heads/master/src/assets/sounds/Britney-Spears-Toxic.mp3',
     duration: 212,
     metaData: {
       title: 'Toxic',
@@ -65,7 +65,7 @@ export const initialTracks = [
   },
   {
     url:
-      'https://raw.githubusercontent.com/bilalvdemir/winamp-xp/refs/heads/master/demo/mp3/Candan%20Erc%CC%A7etin%20-%20Yalan.mp3',
+      'https://raw.githubusercontent.com/bilalvdemir/winXP/refs/heads/master/src/assets/sounds/Candan-Ercetin-Yalan.mp3',
     duration: 238,
     metaData: {
       title: 'Yalan',
@@ -75,7 +75,7 @@ export const initialTracks = [
   },
   {
     url:
-      'https://raw.githubusercontent.com/bilalvdemir/winamp-xp/refs/heads/master/demo/mp3/CEZA%20-%20Yerli%20Plaka%20(Official%20Audio).mp3',
+      'https://raw.githubusercontent.com/bilalvdemir/winXP/refs/heads/master/src/assets/sounds/CEZA-Yerli-Plaka-(Official-Audio).mp3',
     duration: 275,
     metaData: {
       title: 'Yerli Plaka',
@@ -85,7 +85,7 @@ export const initialTracks = [
   },
   {
     url:
-      'https://raw.githubusercontent.com/bilalvdemir/winamp-xp/refs/heads/master/demo/mp3/Davut%20Gu%CC%88log%CC%86lu%20-%20Katula%20Katula.mp3',
+      'https://raw.githubusercontent.com/bilalvdemir/winXP/refs/heads/master/src/assets/sounds/Davut-Guloglu-Katula-Katula.mp3',
     duration: 238,
     metaData: {
       title: 'Katula Katula',
@@ -95,7 +95,7 @@ export const initialTracks = [
   },
   {
     url:
-      'https://raw.githubusercontent.com/bilalvdemir/winamp-xp/refs/heads/master/demo/mp3/Ebru%20Gu%CC%88ndes%CC%A7%20-%20C%CC%A7ingenem.mp3',
+      'https://raw.githubusercontent.com/bilalvdemir/winXP/refs/heads/master/src/assets/sounds/Ebru-Gundes-Cingenem.mp3',
     duration: 191,
     metaData: {
       title: 'Çingenem',
@@ -105,7 +105,7 @@ export const initialTracks = [
   },
   {
     url:
-      'https://raw.githubusercontent.com/bilalvdemir/winamp-xp/refs/heads/master/demo/mp3/Elvedayla%20su%CC%88slenmis%CC%A7%20bir%20yar%C4%B1n%C4%B1n%20du%CC%88nu%CC%88ndeyim.mp3',
+      'https://raw.githubusercontent.com/bilalvdemir/winXP/refs/heads/master/src/assets/sounds/Elvedayla-suslenmis-bir-yarinin-dunundeyim.mp3',
     duration: 277,
     metaData: {
       title: 'Kayıt',
@@ -115,7 +115,7 @@ export const initialTracks = [
   },
   {
     url:
-      'https://raw.githubusercontent.com/bilalvdemir/winamp-xp/refs/heads/master/demo/mp3/Belki%20Bir%20Gu%CC%88n%20O%CC%88zlersin.mp3',
+      'https://raw.githubusercontent.com/bilalvdemir/winXP/refs/heads/master/src/assets/sounds/Emre-Aydın-Belki-Bir-Gun-Ozlersin.mp3',
     duration: 277,
     metaData: {
       title: 'Belki Bir Gün Özlersin',
@@ -125,7 +125,7 @@ export const initialTracks = [
   },
   {
     url:
-      'https://raw.githubusercontent.com/bilalvdemir/winamp-xp/refs/heads/master/demo/mp3/Eylem%20-%20Aman%20(Official%20Video).mp3',
+      'https://raw.githubusercontent.com/bilalvdemir/winXP/refs/heads/master/src/assets/sounds/Eylem-Aman-(Official-Audio).mp3',
     duration: 198,
     metaData: {
       title: 'Aman',
@@ -135,7 +135,7 @@ export const initialTracks = [
   },
   {
     url:
-      'https://raw.githubusercontent.com/bilalvdemir/winamp-xp/refs/heads/master/demo/mp3/Feridun%20Duzagac%20-%20Beni%20Birakma%20(Video%20Version).mp3',
+      'https://raw.githubusercontent.com/bilalvdemir/winXP/refs/heads/master/src/assets/sounds/Feridun-Duzagac-Beni-Birakma.mp3',
     duration: 257,
     metaData: {
       title: 'Beni Bırakma',
@@ -145,7 +145,7 @@ export const initialTracks = [
   },
   {
     url:
-      'https://raw.githubusercontent.com/bilalvdemir/winamp-xp/refs/heads/master/demo/mp3/Go%CC%88khan%20K%C4%B1rdar%20Yerine%20Sevemem%20%20Music%20Video.mp3',
+      'https://raw.githubusercontent.com/bilalvdemir/winXP/refs/heads/master/src/assets/sounds/Gokhan-Kirdar-Yerine-Sevemem.mp3',
     duration: 262,
     metaData: {
       title: 'Yerine Sevemem',
@@ -155,7 +155,7 @@ export const initialTracks = [
   },
   {
     url:
-      'https://raw.githubusercontent.com/bilalvdemir/winamp-xp/refs/heads/master/demo/mp3/Gu%CC%88lay%20-%20Cesaretin%20Var%20m%C4%B1%20As%CC%A7ka.mp3',
+      'https://raw.githubusercontent.com/bilalvdemir/winXP/refs/heads/master/src/assets/sounds/Gulay-Cesaretin-Var-mi-Aska.mp3',
     duration: 214,
     metaData: {
       title: 'Cesaretin Var mı Aşka',
@@ -165,7 +165,7 @@ export const initialTracks = [
   },
   {
     url:
-      'https://raw.githubusercontent.com/bilalvdemir/winamp-xp/refs/heads/master/demo/mp3/I%CC%87rem%20-%20Hayalet%20Sevgilim%20(Official%20Audio).mp3',
+      'https://raw.githubusercontent.com/bilalvdemir/winXP/refs/heads/master/src/assets/sounds/Irem-Hayalet-Sevgilim-(Official-Audio).mp3',
     duration: 221.44,
     metaData: {
       title: 'Hayalet Sevgilim',
