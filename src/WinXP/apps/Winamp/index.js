bbc7c76b8284646ed3429e4761b23d7882286fc9
bilaldemir <18368833+bilalvdemir@users.noreply.github.com>
2025-06-16 10:36:45 +0300

    fix rename

--- src/WinXP/apps/Winamp/index.js
+++ src/WinXP/apps/Winamp/index.js
@@ -392,8 +392,8 @@ function Winamp({ onClose, onMinimize }) {
       <div
         style={{
           position: 'fixed',
-          left: 600,
-          top: 150,
+          left: 0,
+          top: 0,
           right: 0,
           bottom: 0,
           zIndex: 1000,
